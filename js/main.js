43c02e2f4607e7f011b7d5bb208096e2b9440609
Anthony Hamilton <anthonynhamilton@gmail.com>
2022-12-26 17:38:46 -0600

    made basic framework

--- js/main.js
+++ js/main.js
@@ -2,13 +2,15 @@
 document.querySelector('button').addEventListener('click', getFetch)
 
 function getFetch(){
-  const choice = document.querySelector('input').value
-  const url = 'https://pokeapi.co/api/v2/pokemon/'+choice
+  const url = 'https://www.themealdb.com/api/json/v1/1/random.php'
 
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
         console.log(data)
+        console.log(data.meals[0].strYoutube)
+        document.querySelector('iframe').src = data.meals[0].strYoutube.replace('watch?v=', 'embed/'
+        )
       })
       .catch(err => {
           console.log(`error ${err}`)
